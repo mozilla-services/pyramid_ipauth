bcbd848041c7e33fb4ac8fce7454eff74dba7bed
Ryan Kelly <ryan@rfk.id.au>
2011-10-20 09:48:28 +1100

    Moved ip-based auth into standalone "pyramid_ipauth" package.

---
+++ README.txt
@@ -0,0 +1,6 @@
+pyramid_ipauth
+==============
+
+This is an authentication policy for __pyramid__ that sets identity based
+on the remote IP address of the request.
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = ['pyramid', 'netaddr', 'unittest2']
+
+setup(name='pyramid_ipauth',
+      version='0.1',
+      description='pyramid_ipauth',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons authentication',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pyramid_ipauth",
+      paster_plugins=['pyramid'])
