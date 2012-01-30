3380995172cd042e11ec73d7641dd160d6ce371f
Ryan Kelly <rkelly@mozilla.com>
2012-01-30 11:47:11 +1100

    Update license to MPL 2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid', 'netaddr', 'unittest2']
 
 setup(name='pyramid_ipauth',
-      version='0.1.0',
+      version='0.1.1',
       description='pyramid_ipauth',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
