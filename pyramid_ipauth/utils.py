9b85db3408a9f881c130e1f62669e4f3aa1e24c4
Scott Searcy <ssearcy@roguecu.org>
2016-09-20 15:29:01 -0700

    Fixed FutureWarning in Python 3.5 that "split() requires a non-empty pattern match"

--- pyramid_ipauth/utils.py
+++ pyramid_ipauth/utils.py
@@ -17,7 +17,7 @@ from pyramid.compat import integer_types, string_types
 
 #  This is used to split a string on an optional comma,
 #  followed by any amount of whitespace.
-_COMMA_OR_WHITESPACE = re.compile(r",?\s*")
+_COMMA_OR_WHITESPACE = re.compile(r"(,\s*|\s+)")
 
 
 def get_ip_address(request, proxies=None):
